fd5b4bcd2a6de2315393106f194805e2b6afccd7
AEROTECH\cbball <cbball@aerotech.com>
2018-06-01 08:51:58 -0400

    Implementing the features from Module 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,47 @@ namespace GradeBook.GradeBooks
         {
             this.Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Not enough students for a Ranked Gradebook.");
+            }
+
+            //Returning the letter grade of the average grade
+            //First, calculates the number of students with an average grade larger than averageGrade
+            int numGreaterThanAvg = 0;
+            foreach (var student in Students)
+            {
+                if (student.AverageGrade > averageGrade)
+                {
+                    numGreaterThanAvg++;
+                }
+            }
+
+            //Then, uses that number to calculate the letter grade
+            if (numGreaterThanAvg < Students.Count / 5)
+            {
+                return 'A';
+            }
+
+            else if (numGreaterThanAvg < 2 * (Students.Count / 5))
+            {
+                return 'B';
+            }
+
+            else if (numGreaterThanAvg < 3 * (Students.Count / 5))
+            {
+                return 'C';
+            }
+
+            else if (numGreaterThanAvg < 4 * (Students.Count / 5))
+            {
+                return 'D';
+            }
+
+            return 'F';
+        }
     }
 }
